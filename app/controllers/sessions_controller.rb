2ee20f5696d694f002c18640579363c6493c521f
App Academy Student <student@appacademy.io>
2014-10-28 10:58:42 -0400

    User model, controller, session controller, new user views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(user_params)
+    if @user
+      session[:token] = @user.reset_session_token!
+      flash[:notice] = "Successfully logged in"
+      redirect_to root_url
+    else
+      flash[:errors] = "Incorrect username or password."
+      render :new
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:token] = nil
+    redirect_to new_session_url
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Successfully created account!"
+      redirect_to root_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.nfind(params[:id])
+    render :edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      flash[:notice] = "Successfully updated profile"
+      redirect_to :show
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :breed, :country, :zip, :email)
+  end
+
 end
